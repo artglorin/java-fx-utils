47d0715a20f13e3cf05b639bc552b06d470be059
V.Verminsky <mail@artglorin.com>
2017-03-23 22:31:01 +0300

    first commit

---
+++ src/main/java/com/artglorin/javaFxUtil/LayoutUtil.java
@@ -0,0 +1,63 @@
+package com.artglorin.javaFxUtil;
+
+import javafx.fxml.FXMLLoader;
+import org.slf4j.Logger;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
+
+import static org.slf4j.LoggerFactory.getLogger;
+
+/**
+ * Created by V.Verminsky on 23.03.2017.
+ */
+public class LayoutUtil {
+
+    private static final Logger LOGGER = getLogger(LayoutUtil.class);
+
+    private LayoutUtil(){}
+
+    private static URL getResource(String name) {
+        return LayoutUtil.class.getClassLoader().getResource(name);
+    }
+
+    public static <T> void setUpView(String fxml, Consumer<T> action) {
+        final URL resource = getResource(fxml + ".fxml");
+        if (resource != null) {
+            try {
+                action.accept(FXMLLoader.load(resource));
+            } catch (IOException e) {
+                LOGGER.error("cannot load fxml file: {}.fxml\nException: {}", fxml,  e);
+            }
+        }
+    }
+
+    public static <T> void setUpController(String fxml, Consumer<T> consumer) {
+        final URL resource = getResource(fxml + ".fxml");
+        if (resource != null) {
+            final FXMLLoader loader = new FXMLLoader();
+            try {
+                loader.load(resource.openStream());
+                consumer.accept(loader.getController());
+            } catch (IOException e) {
+                LOGGER.error("cannot load fxml file: {}.fxml\nException: {}", fxml,  e);
+            }
+        }
+    }
+
+    public static <C,V> void setUpControllerAndView(String fxml, BiConsumer<C,V> consumer) {
+        final URL resource = getResource(fxml + ".fxml");
+        if (resource != null) {
+            final FXMLLoader loader = new FXMLLoader();
+            try {
+                V view = loader.load(resource.openStream());
+                consumer.accept(loader.getController(), view);
+            } catch (IOException e) {
+                LOGGER.error("cannot load fxml file: {}.fxml\nException: {}", fxml,  e);
+            }
+        }
+    }
+
+}
