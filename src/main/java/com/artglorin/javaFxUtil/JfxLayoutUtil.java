0106965d0d4a3bf8cb842a85b7bd8d00b4024215
V.Verminsky <mail@artglorin.com>
2017-03-26 10:39:56 +0300

    add ControllerFactory

--- src/main/java/com/artglorin/javaFxUtil/JfxLayoutUtil.java
+++ src/main/java/com/artglorin/javaFxUtil/JfxLayoutUtil.java
@@ -1,10 +1,13 @@
 package com.artglorin.javaFxUtil;
 
 import javafx.fxml.FXMLLoader;
+import javafx.util.Callback;
 import org.slf4j.Logger;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URL;
+import java.util.Optional;
 import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 
@@ -13,53 +16,53 @@ import static org.slf4j.LoggerFactory.getLogger;
 /**
  * Created by V.Verminsky on 23.03.2017.
  */
+@SuppressWarnings("WeakerAccess")
 public class JfxLayoutUtil {
 
     private static final Logger LOGGER = getLogger(JfxLayoutUtil.class);
+    private static Callback<Class<?>, Object> controllerFactory;
 
-    private JfxLayoutUtil(){}
-
-    private static URL getResource(String name) {
-        return JfxLayoutUtil.class.getClassLoader().getResource(name);
+    private static Optional<URL> getResource(String name) {
+        final URL resource = JfxLayoutUtil.class.getClassLoader().getResource(name + ".fxml");
+        if (resource == null) {
+            LOGGER.error("cannot load fxml file: {}.fxml\nException: {}", name);
+        }
+        return Optional.ofNullable(resource);
     }
 
-    public static <T> void setUpView(String fxml, Consumer<T> action) {
-        final URL resource = getResource(fxml + ".fxml");
-        if (resource != null) {
-            try {
-                action.accept(FXMLLoader.load(resource));
-            } catch (IOException e) {
-                LOGGER.error("cannot load fxml file: {}.fxml\nException: {}", fxml,  e);
-            }
+    private static FXMLLoader newLoader() {
+        final FXMLLoader loader = new FXMLLoader();
+        if (controllerFactory != null) {
+            loader.setControllerFactory(controllerFactory);
         }
+        return loader;
+    }
+
+    public static void setControllerFactory(Callback<Class<?>, Object> controllerFactory) {
+        JfxLayoutUtil.controllerFactory = controllerFactory;
     }
 
     public static <T> void setUpController(String fxml, Consumer<T> consumer) {
-        final URL resource = getResource(fxml + ".fxml");
-        if (resource != null) {
-            final FXMLLoader loader = new FXMLLoader();
-            try {
-                loader.load(resource.openStream());
-                consumer.accept(loader.getController());
-            } catch (IOException e) {
-                LOGGER.error("cannot load fxml file: {}.fxml\nException: {}", fxml,  e);
-            }
-        }
+        setUpControllerAndView(fxml, (BiConsumer<T, ? extends Object>) (controller, view) -> consumer.accept(controller));
     }
 
-    public static <C,V> void setUpControllerAndView(String fxml, BiConsumer<C,V> consumer) {
-        final URL resource = getResource(fxml + ".fxml");
-        if (resource != null) {
-            final FXMLLoader loader = new FXMLLoader();
-            try {
-                V view = loader.load(resource.openStream());
+    public static <C, V> void setUpControllerAndView(String fxml, BiConsumer<C, V> consumer) {
+        getResource(fxml).ifPresent(url -> {
+            final FXMLLoader loader = newLoader();
+            try (InputStream inputStream = url.openStream()) {
+                V view = loader.load(inputStream);
                 consumer.accept(loader.getController(), view);
             } catch (IOException e) {
-                LOGGER.error("cannot load fxml file: {}.fxml\nException: {}", fxml,  e);
+                LOGGER.error("cannot load fxml file: {}.fxml\nException: {}", fxml, e);
             }
-        } else {
-            LOGGER.error("cannot load fxml file: {}.fxml\nException: {}", fxml);
-        }
+        });
+    }
+
+    public static <T> void setUpView(String fxml, Consumer<T> action) {
+        setUpControllerAndView(fxml, (BiConsumer<? extends Object, T>) (controller, view) -> action.accept(view));
+    }
+
+    private JfxLayoutUtil() {
     }
 
 }
